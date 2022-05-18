57a185a2d6df7a9650fc946544b8933bace0a893
deepanshu mehra <deepanshumehra2102@gmail.com>
2022-05-18 22:13:55 +0530

    commiting

---
+++ src/components/Apple.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "./Apple.css"
+
+function Apple({date,logo,heading,subheading,devices,color}) {
+  return (
+    <div className='apple' style={{
+        'backgroundColor': color
+    }}>
+        <div className='logo-div'>
+            <div className='date'>{date}</div>
+            <div>
+                <img style={{'width' : "40%" ,"margin" : "10px"}} src={logo} alt={heading} />
+            </div>
+        </div>
+        <div>
+         <p className='case-stu'>Case Study</p>
+         <p className="heading">{heading}</p>
+         <p className="sub-heading">{subheading}</p>
+        </div>
+        <div className='arrow-div'>
+            <div>
+              <p className='device'>MacOS - {devices}</p>
+            </div>
+            <div>
+                <p className='arrow-sign'>→</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Apple
